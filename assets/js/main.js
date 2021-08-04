eec907f29a6edb4b1d8906bab3f5aeaa74e65218
James Hodgson <jim@jimhodgson.com>
2021-08-04 10:46:23 -0400

    video update

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,5 +113,26 @@
 				});
 
 			});
+			
+				// Main Sections: Two.
+
+		// Lightbox gallery.
+			$window.on('load', function() {
+
+				$('#twopointfive').poptrox({
+					caption: function($a) { return $a.next('h3').text(); },
+					overlayColor: '#2c2c2c',
+					overlayOpacity: 0.85,
+					popupCloserText: '',
+					popupLoaderText: '',
+					selector: '.work-item a.image',
+					usePopupCaption: true,
+					usePopupDefaultStyling: false,
+					usePopupEasyClose: false,
+					usePopupNav: true,
+					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+				});
+
+			});
 
 })(jQuery);
